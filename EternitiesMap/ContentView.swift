1752422e229090bbdac7c00faf031cd6fa5a7f80
Chad Zeluff <czeluff@gmail.com>
2021-10-05 21:05:15 -0700

    attempt at a start

---
+++ EternitiesMap/ContentView.swift
@@ -0,0 +1,57 @@
+//
+//  ContentView.swift
+//  EternitiesMap
+//
+//  Created by Chad Zeluff on 10/3/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @State private var scrollTarget: Int?
+    // Work-around to firing a change in the scrollPosition. If the button were IN the ScrollViewReader context, I could do:
+    // proxy.scrollTo(target, anchor: center)
+    // as the button action.
+    
+    var body: some View {
+        ZStack(alignment: .bottomTrailing) {
+            GeometryReader { reader in
+                // Wish I could put this scrollView into a local variable, but I can't because it utilizes GeometryReader
+                ScrollView(.horizontal, showsIndicators: false) {
+                    ScrollViewReader { proxy in
+                        HStack(spacing: 0) { // Could just as easily be a ZStack
+                            Text("Hello")
+                                .frame(width: reader.size.width, height: reader.size.height)
+                                .background(Color.red)
+                                .id(1)
+                            
+                            Text("World")
+                                .frame(width: reader.size.width, height: reader.size.height)
+                                .background(Color.yellow)
+                                .id(2)
+                        }
+                        .onChange(of: scrollTarget) { target in
+                            scrollTarget = nil
+                            withAnimation {
+                                proxy.scrollTo(target, anchor: .center)
+                            }
+                        }
+                    }
+                }
+                .disabled(true)
+            }
+            
+            Button("Click Me") {
+                scrollTarget = 2
+            }
+            .padding(20)
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView().previewInterfaceOrientation(.landscapeLeft)
+    }
+}
---
+++ EternitiesMap/EternitiesMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  EternitiesMapApp.swift
+//  EternitiesMap
+//
+//  Created by Chad Zeluff on 10/3/21.
+//
+
+import SwiftUI
+
+@main
+struct EternitiesMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
